1ab75bd3accc7b7a039dd9a5fc6e5a3b0b5413ff
Nikkolas <jn147860@gmail.com>
2026-03-01 23:42:20 -0500

    Añado avances de conexion con dos vistas

--- elegant-cut-vite-back/src/main.ts
+++ elegant-cut-vite-back/src/main.ts
@@ -7,11 +7,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-    credentials: true,
-  });
-
   app.setGlobalPrefix('api');
 
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
@@ -25,6 +20,14 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  // Esto habilita cors para que el navegador le de acceso al front end
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
