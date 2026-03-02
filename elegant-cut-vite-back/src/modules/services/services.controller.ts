bcbd1a5ec996548926dd1fd15f5eda076ac4c527
Nikkolas <jn147860@gmail.com>
2026-03-01 22:23:13 -0500

    Añadí todos los métodos en los módulos

--- elegant-cut-vite-back/src/modules/services/services.controller.ts
+++ elegant-cut-vite-back/src/modules/services/services.controller.ts
@@ -1,17 +1,25 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { ServicesService } from './services.service';
+import { CrearServicioDto } from './dto/create-servicio.dto';
 
 @Controller('services')
 export class ServicesController {
     constructor(private readonly servicesService: ServicesService) { }
-
+    // metodos get
     @Get()
     async getAll() {
         return this.servicesService.findAll();
     }
 
+    @Get()
+    async obtenerServicios() {
+        return this.servicesService.obtenerServicios();
+    }
+
+    //metodos post
+
     @Post()
-    async create(@Body() data: any) {
-        return this.servicesService.create(data);
+    async crearServicio(@Body() crearServicioDto: CrearServicioDto) {
+        return this.servicesService.crearServicio(crearServicioDto);
     }
 }
--- elegant-cut-vite-back/src/modules/services/services.service.ts
+++ elegant-cut-vite-back/src/modules/services/services.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { ServicesRepository } from './services.repository';
+import { PrismaService } from '../../prisma/prisma.service';
+import { CrearServicioDto } from './dto/create-servicio.dto';
 
 @Injectable()
 export class ServicesService {
-    constructor(private readonly servicesRepo: ServicesRepository) { }
+    constructor(private readonly servicesRepo: ServicesRepository, private readonly prisma: PrismaService) { }
 
     async findAll() {
         return this.servicesRepo.findAll();
@@ -11,5 +13,17 @@ export class ServicesService {
 
     async create(data: any) {
         return this.servicesRepo.create(data);
+    }
+
+    async obtenerServicios() {
+        return this.prisma.servicios.findMany();
+    }
+
+    //Este lo susamos para crear osea post
+
+    async crearServicio(dato: CrearServicioDto) {
+        return await this.prisma.servicios.create({
+            data: dato,
+        })
     }
 }
