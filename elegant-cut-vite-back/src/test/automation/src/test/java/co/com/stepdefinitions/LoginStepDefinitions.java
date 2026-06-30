620a91ee0ed78ebe0b627ef18903c1a4b392c759
Nikkolas <jn147860@gmail.com>
2026-06-30 16:52:34 -0500

    Bot-completo

--- elegant-cut-vite-back/src/test/automation/src/test/java/co/com/stepdefinitions/LoginStepDefinitions.java
+++ elegant-cut-vite-back/src/test/automation/src/test/java/co/com/stepdefinitions/LoginStepDefinitions.java
@@ -1,17 +1,23 @@
 package co.com.stepdefinitions;
 
 import co.com.tasks.DoLogin;
+import co.com.questions.ElIndex;
+import co.com.userinterfaces.IndexPage;
 import io.cucumber.java.Before;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
+import io.cucumber.java.es.Cuando;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.ensure.Ensure;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 import net.serenitybdd.annotations.Managed;
 import org.openqa.selenium.WebDriver;
-import net.serenitybdd.screenplay.ensure.Ensure;
-import co.com.userinterfaces.DashboardPage;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
+import static org.hamcrest.Matchers.equalTo;
 
 public class LoginStepDefinitions {
 
@@ -25,22 +31,59 @@ public class LoginStepDefinitions {
         user.can(BrowseTheWeb.with(herBrowser));
     }
 
-    @Given("que el usuario se encuentra en la pagina de inicio de sesion")
+    @Dado("que el usuario se encuentra en la pagina de inicio de sesion")
     public void queElUsuarioSeEncuentraEnLaPaginaDeInicioDeSesion() {
         user.wasAbleTo(Open.url("http://localhost:5173/login"));
+        pause(3);
     }
 
-    @When("el ingresa las credenciales de acceso correctas")
+    @Cuando("el ingresa las credenciales de acceso correctas")
     public void elIngresaLasCredencialesDeAccesoCorrectas() {
         user.attemptsTo(
                 DoLogin.withCredentials("pal", "123456"));
+        pause(3);
     }
 
-    @Then("el deberia ver la pantalla principal del sistema")
+    @Entonces("el deberia ver la pantalla principal del sistema")
     public void elDeberiaVerLaPantallaPrincipalDelSistema() {
-        // La prueba se asegura (Ensure) de que el título del Dashboard sea visible en
-        // pantalla
         user.attemptsTo(
-                Ensure.that(DashboardPage.MAIN_TITLE).isDisplayed());
-    } // <-- LLAVE PARA CERRAR EL MÉTODO!
-} // Aquí cierra la clase LoginStepDefinitions
+                WaitUntil.the(IndexPage.DASHBOARD_TITLE, isVisible())
+                        .forNoMoreThan(10).seconds(),
+                Ensure.that(IndexPage.DASHBOARD_TITLE).isDisplayed());
+        pause(3);
+    }
+
+    // index.feature
+    @Dado("que el usuario esta en la pagina de login")
+    public void queElUsuarioEstaEnLaPaginaDeLogin() {
+        user.wasAbleTo(Open.url("http://localhost:5173/login"));
+        pause(3);
+    }
+
+    @Cuando("el usuario ingresa sus credenciales validas")
+    public void elUsuarioIngresaSusCredencialesValidas() {
+        user.attemptsTo(
+                DoLogin.withCredentials("pal", "123456"));
+        pause(3);
+    }
+
+    @Entonces("deberia ver la pagina de inicio index")
+    public void deberiaVerLaPaginaDeInicioIndex() {
+        user.attemptsTo(
+                WaitUntil.the(IndexPage.DASHBOARD_TITLE, isVisible())
+                        .forNoMoreThan(10).seconds());
+        pause(3);
+        user.should(
+                seeThat(ElIndex.seaVisible(), equalTo(true)));
+    }
+
+    // Utilidad
+
+    private void pause(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
