7ea5df8185014f030904e1217cfb4eda75f3cc2a
Angeldev3001 <angellealmolina30@gmail.com>
2026-08-20 01:01:16 -0500

    Revert "Merge pull request #22 from ElegantCut/feature/RF-015"
    This reverts commit 96cf5be655ba43b0ee3bc959455277c4657686d0, reversing
    changes made to f35daff0af1fee169011ced82428f37cd2049ed1.

--- elegant-cut-vite-back/src/modules/email/email.module.ts
+++ elegant-cut-vite-back/src/modules/email/email.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
-import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
   providers: [EmailService],
   exports: [EmailService],
 })
